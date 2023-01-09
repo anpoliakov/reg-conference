6fdc5178f793076f0ac22f7aecf5002887c703ee
anpoliakov <Andrewlybimenko@gmail.com>
2023-01-09 22:49:58 +0300

    add a commit of the initial changes

--- src/main/java/servlets/TestServlet.java
+++ src/main/java/servlets/TestServlet.java
@@ -1,12 +1,14 @@
 package servlets;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+@WebServlet("/test-2")
 public class TestServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
