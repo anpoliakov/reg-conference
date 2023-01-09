1cb616d4f3f3a263a51b9de7bfe84d2df85fef07
anpoliakov <Andrewlybimenko@gmail.com>
2023-01-09 22:33:53 +0300

    add initial commit

---
+++ src/main/java/servlets/TestServlet.java
@@ -0,0 +1,21 @@
+package servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class TestServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.println("<html>");
+        writer.println("<h1>Hello from your servlet</h1>");
+        writer.println("</html>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {}
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>START PAGE</title>
+  </head>
+  <body>
+    <h1 color="red"> START PAGE </h1>
+  </body>
+</html>
