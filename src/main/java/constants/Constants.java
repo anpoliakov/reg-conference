ae7d6a480f2ffc42cedbc3dd921aaa06276ed2cc
anpoliakov <Andrewlybimenko@gmail.com>
2023-01-13 23:34:08 +0300

    add constants for data base

--- src/main/java/constants/Constants.java
+++ src/main/java/constants/Constants.java
@@ -4,11 +4,16 @@ public final class Constants {
     public static final String MESSAGE = "message";
     public static final String LOGIN = "login";
     public static final String PASSWORD = "password";
+    public static final String DRIVER_NAME = "org.postgresql.Driver";
 
     public static final String LOGIN_JSP = "/login.jsp";
     public static final String EMPTY = "";
 
-
     public static final String MESS_NULL = "fields are null";
     public static final String MESS_EMPTY = "fields are empty";
+
+    //не безопасно так хранить (тестовая база)
+    public static final String URL_DB = "jdbc:postgresql://localhost:5432/reg_conf_db";
+    public static final String USER_DB = "postgres";
+    public static final String PASS_DB = "Qwert070123";
 }
