7ba34afdd7e2379bc5dcc3768d3f8b9ab0028728
anpoliakov <Andrewlybimenko@gmail.com>
2023-01-15 21:17:21 +0300

    add a functional of query to data base

--- src/main/java/model/impl/UserImpl.java
+++ src/main/java/model/impl/UserImpl.java
@@ -1,21 +1,35 @@
 package model.impl;
 
+import constants.SQLConstants;
 import model.beans.User;
+import model.db.ConnectionManager;
 import model.interfaces.IUserDAO;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class UserImpl implements IUserDAO {
 
-    public User getUser(String login, String password) {
-        Connection con = null;
+    public User getUser(String login, String password) throws SQLException {
+        Connection conn = null;
         PreparedStatement pst = null;
         ResultSet rs = null;
         User user = null;
 
-        //TODO: организовать запрос в БД
-        return null;
+        conn = ConnectionManager.createConnection();
+        pst = conn.prepareStatement(SQLConstants.SELECT_USER);
+        pst.setString(1, login);
+        pst.setString(2, password);
+        rs = pst.executeQuery();
+
+        while (rs.next()){
+            int id_from_db = rs.getInt(SQLConstants.ID_LABEL);
+            String login_from_db = rs.getString(SQLConstants.LOGIN_LABEL);
+            user = new User(id_from_db, login_from_db);
+        }
+
+        return user;
     }
 }
