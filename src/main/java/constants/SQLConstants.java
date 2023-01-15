1371279e25e145dbed302fb50d3b6a00b8157a21
anpoliakov <Andrewlybimenko@gmail.com>
2023-01-15 21:11:51 +0300

    add a functional of query in data base

---
+++ src/main/java/constants/SQLConstants.java
@@ -0,0 +1,7 @@
+package constants;
+
+public class SQLConstants {
+    public static final String SELECT_USER = "SELECT * FROM users where login=? and password=?";
+    public static final String ID_LABEL = "id";
+    public static final String LOGIN_LABEL = "login";
+}
--- src/main/java/model/interfaces/IUserDAO.java
+++ src/main/java/model/interfaces/IUserDAO.java
@@ -2,6 +2,8 @@ package model.interfaces;
 
 import model.beans.User;
 
+import java.sql.SQLException;
+
 public interface IUserDAO {
-    User getUser(String login, String password);
+    User getUser(String login, String password) throws SQLException;
 }
