2568731da8952205aa7bd16e4f0707c24695492a
anpoliakov <Andrewlybimenko@gmail.com>
2023-01-16 21:51:45 +0300

    delete empty constructor

--- src/main/java/controllers/AbstractController.java
+++ src/main/java/controllers/AbstractController.java
@@ -10,17 +10,13 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class AbstractController extends HttpServlet{
-    public AbstractController() {
-        super();
-    }
-
     protected void jump(HttpServletRequest request, HttpServletResponse response, String url) throws ServletException, IOException {
         RequestDispatcher rd = request.getRequestDispatcher(url);
         rd.forward(request,response);
     }
 
     protected void jumpError(HttpServletRequest request, HttpServletResponse response, String url, String message) throws ServletException, IOException {
-        request.setAttribute(Constants.MESSAGE,message);
+        request.setAttribute(Constants.MESSAGE, message);
         jump(request,response,url);
     }
 }
