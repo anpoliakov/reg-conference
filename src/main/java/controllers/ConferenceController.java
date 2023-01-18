858ac4d00cf5614539ea7de27ee5f51372278a2b
anpoliakov <Andrewlybimenko@gmail.com>
2023-01-18 23:05:52 +0300

    add a new controller - ConferenceController

---
+++ src/main/java/controllers/ConferenceController.java
@@ -0,0 +1,46 @@
+package controllers;
+
+import constants.Constants;
+import model.beans.Conference;
+import model.enums.EnumManager;
+import model.impl.ConferenceImpl;
+import model.interfaces.IConferenceDAO;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+@WebServlet ("/conf")
+public class ConferenceController extends AbstractController{
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String param = request.getParameter(Constants.SECTION);
+
+        if (param == null) {
+            param = Constants.TODAY_SECT;
+        }
+
+        try {
+            Enum<?> section = EnumManager.getKindConf(param);
+            IConferenceDAO confDAO = new ConferenceImpl();
+            List<Conference> conferences = confDAO.getConferences(section);
+            HttpSession session = request.getSession();
+            session.setAttribute(Constants.CONF_LIST, conferences);
+            jump(request, response, Constants.INDEX_JSP);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            jumpError(request, response, Constants.INDEX_JSP, e.getMessage());
+        }
+        return;
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
+}
