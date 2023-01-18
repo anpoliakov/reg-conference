081cd84965b988ad0e4b1638230eddcef832b557
anpoliakov <Andrewlybimenko@gmail.com>
2023-01-18 23:07:12 +0300

    fix block of code "extends"

--- src/main/java/controllers/LogoutController.java
+++ src/main/java/controllers/LogoutController.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet("/logout")
-public class LogoutController extends HttpServlet {
+public class LogoutController extends AbstractController {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession session = request.getSession();
         session.invalidate();
