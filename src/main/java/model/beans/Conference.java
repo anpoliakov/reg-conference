8d039f1669469ed214906b5bc15121c9c4b5bac7
anpoliakov <Andrewlybimenko@gmail.com>
2023-01-24 18:20:57 +0300

    rename fields, add new constructor and create method for parse date

--- src/main/java/model/beans/Conference.java
+++ src/main/java/model/beans/Conference.java
@@ -1,27 +1,33 @@
 package model.beans;
 
+import constants.Constants;
+
 import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.List;
 
 public class Conference {
     private int id;
-    private String name;
+    private String title;
+    private String descr;
     private String place;
     private Date date;
     private List<Event> events;
 
-    public Conference(int id, String name, String place, Date date, List<Event> events) {
+    public Conference(String title, String descr, String place, String date) throws ParseException {
         super();
-        this.id = id;
-        this.name = name;
+        this.title = title;
+        this.descr = descr;
         this.place = place;
-        this.date = date;
-        this.events = events;
+        setDate(date);
     }
 
-    public Conference(int id, String name, String place, Date date) {
+    public Conference(int id, String title, String descr, String place, Date date) {
+        super();
         this.id = id;
-        this.name = name;
+        this.title = title;
+        this.descr = descr;
         this.place = place;
         this.date = date;
     }
@@ -34,12 +40,20 @@ public class Conference {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getTitle() {
+        return title;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescr() {
+        return descr;
+    }
+
+    public void setDescr(String descr) {
+        this.descr = descr;
     }
 
     public String getPlace() {
@@ -56,6 +70,11 @@ public class Conference {
 
     public void setDate(Date date) {
         this.date = date;
+    }
+
+    public void setDate(String date) throws ParseException {
+        SimpleDateFormat format = new SimpleDateFormat(Constants.DATE_PATTERN);
+        this.date = new Date (format.parse(date).getTime());
     }
 
     public List<Event> getEvents() {
