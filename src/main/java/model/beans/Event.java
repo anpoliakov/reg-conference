c1e0cf31ec77376e16ab298b19fbe800e42da61e
anpoliakov <Andrewlybimenko@gmail.com>
2023-01-25 22:42:50 +0300

    add a method for parsing time

--- src/main/java/model/beans/Event.java
+++ src/main/java/model/beans/Event.java
@@ -1,16 +1,31 @@
 package model.beans;
 
+import constants.Constants;
+
 import java.sql.Time;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class Event {
     private int id;
     private String name;
     private Time time;
 
+    public Event(String name, String time) throws ParseException {
+        this.name = name;
+        setTime(time);
+    }
+
     public Event(int id, String name, Time time) {
         this.id = id;
         this.name = name;
         this.time = time;
+    }
+
+    public Event(int id, String name, String time) throws ParseException {
+        this.id = id;
+        this.name = name;
+        setTime(time);
     }
 
     public int getId() {
@@ -33,7 +48,8 @@ public class Event {
         return time;
     }
 
-    public void setTime(Time time) {
-        this.time = time;
+    public void setTime(String time) throws ParseException {
+        SimpleDateFormat format = new SimpleDateFormat(Constants.TIME_PATTERN);
+        this.time = new Time(format.parse(time).getTime());
     }
 }
