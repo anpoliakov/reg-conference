36ba9d5226c956fd571d3c33c4f96d8406912ff7
anpoliakov <Andrewlybimenko@gmail.com>
2023-01-25 22:37:54 +0300

    add working constants sql for a table of conferences

--- src/main/java/constants/SQLConstants.java
+++ src/main/java/constants/SQLConstants.java
@@ -2,16 +2,16 @@ package constants;
 
 public class SQLConstants {
     public static final String SELECT_USER = "select * from users where login=? and password=?";
-    public static final String INSERT_USER = "insert into users(login, password, firstname, secondname, email) values(?,?,?,?,?)";
+    public static final String INSERT_USER = "insert into users(login, email, firstName, lastName, password) values(?,?,?,?,?)";
     public static final String FOUND_LOGIN = "select id from users where login=?";
 
     public static final String SELECT_CONF = "select * from conferences ";
     public static final String WHERE_ALL = "";
-    public static final String WHERE_TODAY = "where date=CURRENT_DATE";
-    public static final String WHERE_TOMORROW = "where date=DATE_ADD(CURRENT_DATE(), INTERVAL 1 DAY)";
-    public static final String WHERE_SOON ="where date>DATE_ADD(CURRENT_DATE(), INTERVAL 1 DAY)";
-    public static final String WHERE_PAST ="where date<CURRENT_DATE()";
-    public static final String WHERE_CONF_BY_USER = "where idUser = ?";
+    public static final String WHERE_TODAY = "where date = NOW()::DATE";
+    public static final String WHERE_TOMORROW = "where date = CURRENT_DATE + INTERVAL '1 DAY'";
+    public static final String WHERE_SOON ="where date > CURRENT_DATE + INTERVAL '1 DAY'";
+    public static final String WHERE_PAST ="where date < CURRENT_DATE";
+    public static final String WHERE_CONF_BY_USER = "where id = ?";
 
     public static final String SELECT_EVENTS = "select events.id, events.title, events.time from events join conferences on idConf = conferences.id "
             + "where idConf=? order by time";
