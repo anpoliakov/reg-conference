c7d2911ef4295a8139deff8a1064deff9066efaf
anpoliakov <Andrewlybimenko@gmail.com>
2023-01-25 22:39:41 +0300

    add a future DAO methods

--- src/main/java/model/interfaces/IConferenceDAO.java
+++ src/main/java/model/interfaces/IConferenceDAO.java
@@ -1,11 +1,21 @@
 package model.interfaces;
 
 import model.beans.Conference;
+import model.beans.User;
 
 import java.sql.SQLException;
+import java.text.ParseException;
 import java.util.List;
 
 public interface IConferenceDAO {
-    List<Conference> getConferences(Enum <?> select) throws SQLException;
-    int fillConferenceEvents(List<Conference> conferences, String idConf) throws SQLException;
+    List<Conference> getConferences(Enum <?> select, User user) throws SQLException;
+    int fillConferenceEvents(List<Conference> conferences, String idConf) throws SQLException, ParseException;
+
+    /*
+    *
+    int addConference(Conference conference, User user) throws SQLException;
+	void addConferenceEvents(List<Event> events, int idConf) throws SQLException;
+	void removeConferences(String[] idConferences, User user) throws SQLException;
+    *
+    * */
 }
