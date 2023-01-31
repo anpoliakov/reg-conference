2f6eb5dd2c0e3c4636de23a3d9058b3f81fa2908
anpoliakov <Andrewlybimenko@gmail.com>
2023-01-31 21:07:36 +0300

    rename method

--- src/main/java/model/enums/SectionManager.java
+++ src/main/java/model/enums/SectionManager.java
@@ -1,7 +1,7 @@
 package model.enums;
 
 public class SectionManager {
-    public static SectionKind getKindConf(String param) {
+    public static SectionKind getSectionKind(String param) {
         try {
             SectionKind kind = SectionKind.valueOf(param.toUpperCase());
             return kind;
