fee52bce4884de4fc872554a14565f2586054a8e
anpoliakov <Andrewlybimenko@gmail.com>
2023-02-20 22:34:22 +0300

    rename fields and add SQL request for select events

--- src/main/java/constants/SQLConstants.java
+++ src/main/java/constants/SQLConstants.java
@@ -13,19 +13,19 @@ public class SQLConstants {
     public static final String WHERE_PAST ="where date < CURRENT_DATE";
     public static final String WHERE_CONF_BY_USER = "WHERE idUser =?";
 
-    public static final String SELECT_EVENTS = "select events.id, events.title, events.time from events join conferences on idConf = conferences.id "
-            + "where idConf=? order by time";
+    public static final String SELECT_EVENTS = "SELECT events.id, events.name, events.time FROM events INNER JOIN conferences ON events.idConf = conferences.id WHERE events.idConf = ? ORDER BY events.time";
 
     public static final String ID_LABEL = "id";
     public static final String LOGIN_LABEL = "login";
     public static final String FNAME_LABEL = "firstName";
     public static final String SNAME_LABEL = "secondName";
     public static final String EMAIL_LABEL = "email";
-    public static final String TITLE_LABLE = "title";
-    public static final String DESCR_LABLE = "descr";
-    public static final String PLACE_LABLE = "place";
-    public static final String DATE_LABLE = "date";
-    public static final String TIME_LABLE = "time";
+    public static final String NAME_LABEL = "name";
+    public static final String TITLE_LABEL = "title";
+    public static final String DESCR_LABEL = "descr";
+    public static final String PLACE_LABEL = "place";
+    public static final String DATE_LABEL = "date";
+    public static final String TIME_LABEL = "time";
 
     public static final String INSERT_CONF = "INSERT INTO conferences (idUser, title, descr, place, date) VALUES (?,?,?,?,?)";
     public static final String INSERT_EVENTS = "INSERT INTO events (idConf, name, time) VALUES (?, ?, ?)";
