503f93a198cb821ddcf44ec7f1424347b2f26ea1
anpoliakov <Andrewlybimenko@gmail.com>
2023-03-14 22:51:56 +0300

    edit constants

--- src/main/java/constants/SQLConstants.java
+++ src/main/java/constants/SQLConstants.java
@@ -5,13 +5,14 @@ public class SQLConstants {
     public static final String INSERT_USER = "insert into users(login, email, firstName, secondName, password) values(?,?,?,?,?)";
     public static final String FOUND_LOGIN = "select id from users where login=?";
 
-    public static final String SELECT_CONF = "SELECT * FROM conferences ";
+    public static final String SELECT_CONF = "SELECT * FROM conferences "; /* обязательно пробел в конце данной строки */
     public static final String WHERE_ALL = "";
-    public static final String WHERE_TODAY = "where date = NOW()::DATE";
-    public static final String WHERE_TOMORROW = "where date = CURRENT_DATE + INTERVAL '1 DAY'";
-    public static final String WHERE_SOON ="where date > CURRENT_DATE + INTERVAL '1 DAY'";
-    public static final String WHERE_PAST ="where date < CURRENT_DATE";
-    public static final String WHERE_CONF_BY_USER = "WHERE idUser =?";
+    public static final String WHERE_TODAY = "WHERE date = NOW()::DATE";
+    public static final String WHERE_TOMORROW = "WHERE date = CURRENT_DATE + INTERVAL '1 DAY'";
+    public static final String WHERE_SOON ="WHERE date > CURRENT_DATE + INTERVAL '1 DAY'";
+    public static final String WHERE_PAST ="WHERE date < CURRENT_DATE";
+    public static final String WHERE_CONF_BY_USER = "WHERE idUser=?";
+    public static final String SELECT_CONF_AND_EVENTS = "SELECT conferences.title, conferences.descr, conferences.place, conferences.date, events.name, events.time FROM events INNER JOIN conferences ON events.idConf = conferences.id WHERE conferences.id = ?";
 
     public static final String SELECT_EVENTS = "SELECT events.id, events.name, events.time FROM events INNER JOIN conferences ON events.idConf = conferences.id WHERE events.idConf = ? ORDER BY events.time";
 
